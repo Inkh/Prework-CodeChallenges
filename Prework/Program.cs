76e71d9aefdc59a14d52b75822005cbadf0591f8
inkh <jhwe92@gmail.com>
2018-10-04 21:40:26 -0700

    Set up solution functions (temp)

--- Prework/Program.cs
+++ Prework/Program.cs
@@ -9,5 +9,20 @@ namespace Prework
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hi there");
         }
+
+        static int Selection(int[] arr, int num)
+        {
+            return 24;
+        }
+
+        static bool LeapYear(int year)
+        {
+            return true;
+        }
+
+        static string PerfectSequence(int[] arr)
+        {
+            return "Hi";
+        }
     }
 }
