8c6c5881e5042647ba67bad828b36f6d6f7cf4d6
inkh <jhwe92@gmail.com>
2018-10-05 15:15:09 -0700

    Finished user inputs portion of selection method.

--- Prework/Program.cs
+++ Prework/Program.cs
@@ -6,10 +6,53 @@ namespace Prework
     {
         static void Main(string[] args)
         {
+            UserInput();
             int[] myArr = { 1, 2, 3 };
             int[] selectArr = { 6, 4, 4, 1, 3 };
+            int[,] sumArray = new int[3, 5] { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 } };
             Console.WriteLine($"Perfect Sequence --- Input: [{string.Join(",", myArr)}]... Expected Output: Yes, Output: {PerfectSequence(myArr)}.");
-            Console.WriteLine($"Selection --- Input: ([{string.Join(",", selectArr)}], 4)... Expected Output: 8, Output: {Selection(selectArr, 6)}.");
+        }
+        static void UserInput()
+        {
+            int[] userArr = new int[5];
+            string userInput;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Input a number between 1 - 10: ");
+                userInput = Console.ReadLine();
+                bool flag = false;
+                while (!flag)
+                {
+                    flag = true;
+                    if (!int.TryParse(userInput, out int result))
+                    {
+                        Console.WriteLine("That was not a number, please try again: ");
+                        userInput = Console.ReadLine();
+                        flag = false;
+                    }
+                    else if (int.Parse(userInput) >= 10 || int.Parse(userInput) <= 1)
+                    {
+                        Console.WriteLine("Number out of range. Please input a number between 1-10: ");
+                        userInput = Console.ReadLine();
+                        flag = false;
+                    }
+                }
+                userArr[i] = int.Parse(userInput);
+            }
+            Console.WriteLine($"[{string.Join(",", userArr)}] is your array. Please select one of the elements within for scoring: ");
+            userInput = Console.ReadLine();
+            bool flagTwo = false;
+            while (!flagTwo)
+            {
+                flagTwo = true;
+                if (!int.TryParse(userInput, out int result))
+                {
+                    Console.WriteLine("That was not a number, please try again: ");
+                    userInput = Console.ReadLine();
+                    flagTwo = false;
+                }
+            }
+            Selection(userArr, int.Parse(userInput));
         }
 
         static int Selection(int[] arr, int num)
@@ -22,6 +65,7 @@ namespace Prework
                     counter++;
                 }
             }
+            Console.WriteLine($"Your array: [{string.Join(",", arr)}]. Your chosen number: {num}. Your Score: {num*counter}");
             return num*counter;
         }
 
