49f4e1e443cec8ef0c656778e361677940d3ad82
inkh <jhwe92@gmail.com>
2018-10-04 22:12:09 -0700

    Perfect sequence completed

--- Prework/Program.cs
+++ Prework/Program.cs
@@ -6,8 +6,9 @@ namespace Prework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hi there");
+            int[] myArr = { 1, 2, 3 };
+            PerfectSequence(myArr);
+            Console.WriteLine($"Input: [{string.Join(",", myArr)}]. Expected Output: Yes, Output: {PerfectSequence(myArr)}.");
         }
 
         static int Selection(int[] arr, int num)
@@ -22,7 +23,18 @@ namespace Prework
 
         static string PerfectSequence(int[] arr)
         {
-            return "Hi";
+            int sum = 0;
+            int product = 1;
+            foreach (int num in arr)
+            {
+                sum += num;
+                product *= num;
+            }
+            if (sum == product)
+            {
+                return "Yes";
+            }
+            return "No";
         }
     }
 }
